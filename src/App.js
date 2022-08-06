c37e7f326c3f722e36083eeb50dce782cd2e2ea3
VASHKOR-DAS <103654975+VASHKOR-DAS@users.noreply.github.com>
2022-08-07 02:02:08 +0800

    use reac for similar look different pattern

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <Person></Person>
+          <Person></Person>
+          <Person></Person>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
 }
+
+function Person () {
+  const personStyle={
+    border: '2px solid red',
+    margin : '20px',
+    padding : '20px',
+  }
+  return (
+    <div style={personStyle}>
+    {/*
+    ai vabeo tag er vitor style lekha jay 
+    <div style={{border: '2px solid red',
+    margin : '10px'}}> 
+    */}
+
+      <h1>Name : Joya Das</h1>
+      <h3>Mother of the year</h3>
+    </div>
+  )
+}
+
+
 
 export default App;
