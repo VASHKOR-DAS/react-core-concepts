871aa63bd0b8948def9cdc31c387dfd47e29fe87
VASHKOR-DAS <103654975+VASHKOR-DAS@users.noreply.github.com>
2022-08-08 02:39:49 +0800

    read data from array

--- src/App.js
+++ src/App.js
@@ -3,13 +3,20 @@
 import './App.css';
 
 function App() {
+  const names = ["Mark Zukar", "Steve Jobs", "Shiv"]
   return (
     <div className="App">
       <header className="App-header">
         <p>
-          <Person name="Mark Zukar" discovered="Facebook"></Person>
+          {/* <Person name="Mark Zukar" discovered="Facebook"></Person>
           <Person name="Steve Jobs" discovered="Apple"></Person>
-          <Person name="Shiv" discovered="Haven"></Person>
+          <Person name="Shiv" discovered="Haven"></Person> */}
+
+          {/* read data from array */}
+          <Person name={names[0]} discovered="Facebook"></Person>
+          <Person name={names[1]} discovered="Apple"></Person>
+          <Person name={names[2]} discovered="Haven"></Person>
+
         </p>
       </header>
     </div>
@@ -30,6 +37,9 @@ function Person (props) {
     <div style={{border: '2px solid red',
     margin : '10px'}}> 
     */}
+
+      {/* <h2>Name : {props.name}</h2>
+      <h3>Discovered : {props.discovered}</h3> */}
 
       <h2>Name : {props.name}</h2>
       <h3>Discovered : {props.discovered}</h3>
