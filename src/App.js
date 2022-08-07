8023d2f93e7d697683b81651193ecc6dcdb61742
VASHKOR-DAS <103654975+VASHKOR-DAS@users.noreply.github.com>
2022-08-08 02:49:46 +0800

    age property onno kothao use na korle default value hisebe none bosiye

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
           <Person name="Shiv" discovered="Haven"></Person> */}
 
           {/* read data from array */}
-          <Person name={names[0]} discovered="Facebook"></Person>
+          <Person name={names[0]} discovered="Facebook" age="56"></Person>
           <Person name={names[1]} discovered="Apple"></Person>
           <Person name={names[2]} discovered="Haven"></Person>
 
@@ -43,6 +43,9 @@ function Person (props) {
 
       <h2>Name : {props.name}</h2>
       <h3>Discovered : {props.discovered}</h3>
+
+      {/* age property onno kothao use na korle default value hisebe none bosiye */}
+      <p>He has done it in {props.age || 'none'} years</p>
     </div>
   )
 }
